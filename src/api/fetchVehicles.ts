8b62a34c99156c822a83c561957c5019be8e3ec6
Sergei Zhilinskij <roadtomars2030@gmail.com>
2023-11-11 01:50:44 +0300

    remove logs

--- src/api/fetchVehicles.ts
+++ src/api/fetchVehicles.ts
@@ -2,8 +2,6 @@ import { URL, QUERY } from '../consts';
 import { Vehicle } from '../interfaces';
 
 export const fetchVehicles = async (): Promise<Vehicle[]> => {
-  console.log('FETCHING');
-
   const response = await fetch(URL, {
     method: 'POST',
     headers: {
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -12,8 +12,6 @@ export const CardList = () => {
     if (totalPages < page) setPage(totalPages);
   }, [totalPages, page]);
 
-  console.log('list render', totalPages, page, filteredVehicles);
-
   return (
     <div className="flex flex-col gap-4">
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-center justify-items-center">
